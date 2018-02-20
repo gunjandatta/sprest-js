89a8b3ff1510133abf9d3d462784a24ee5da5b7e
Gunjan Datta <me@dattabase.com>
2018-02-19 21:39:02 -0500

    Created a dist.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require("path");
+var webpack = require("webpack");
+
+// WebPack Configuration
+module.exports = {
+    entry: "./build/index.js",
+    output: {
+        filename: "dist/gd-sprest-js.js",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.tsx?/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["es2015"]
+                        }
+                    },
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            }
+        ]
+    }
+}
