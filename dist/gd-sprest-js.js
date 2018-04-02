684fd1c0329e8bf1703a85e5433772b256c50b9d
Gunjan Datta <me@dattabase.com>
2018-04-02 09:52:08 -0400

    Updated the core libraries.

--- dist/gd-sprest-js.js
+++ dist/gd-sprest-js.js
@@ -11395,6 +11395,7 @@ var BaseRequest = /** @class */ (function (_super) {
         targetInfo.data = methodInfo.body;
         targetInfo.defaultToWebFl = typeof (targetInfo.defaultToWebFl) === "undefined" && this.base ? this.base.targetInfo.defaultToWebFl : targetInfo.defaultToWebFl;
         targetInfo.method = methodInfo.requestMethod;
+        targetInfo.requestType = methodConfig.requestType;
         // See if we are replacing the endpoint
         if (methodInfo.replaceEndpointFl) {
             // Replace the endpoint
@@ -11778,6 +11779,11 @@ var BaseExecution = /** @class */ (function (_super) {
                     _this.base.waitFlags[_this.responseIndex] = true;
                 }
             });
+        }
+        // See if this is a query request
+        if (this.targetInfo.requestType == _1.RequestType.OData) {
+            // Return the parent
+            return this.parent;
         }
         // Return this object
         return this;
@@ -16014,7 +16020,7 @@ var Mapper = __webpack_require__(6);
  * SharePoint REST Library
  */
 exports.$REST = {
-    __ver: 3.75,
+    __ver: 3.77,
     ContextInfo: Lib.ContextInfo,
     DefaultRequestToHostFl: false,
     Helper: {
